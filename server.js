98c074b1f2f38057100b43d11dd14201c2abd52e
Qasid Ali <qasidali856@gmail.com>
2024-01-30 16:39:37 -0500

    modified

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 // instance
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 // route 
 const product_route = require("./routes/products")
 // connection
